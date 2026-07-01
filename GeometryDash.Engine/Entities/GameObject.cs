9fe23c4e6f5f7850fcf88fcf8d88611d15297fb8
Ondřej Honc <ondra.honc1@gmail.com>
2026-07-01 16:30:09 +0200

    add: basic properties to GameObject

--- GeometryDash.Engine/Entities/GameObject.cs
+++ GeometryDash.Engine/Entities/GameObject.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GeometryDash.Engine.Entities
+﻿namespace GeometryDash.Engine.Entities
 {
   internal class GameObject
   {
+    public enum ObjectType
+    {
+      SolidBlock,
+      Spike,
+      JumpPad
+    }
+    
+    public float PosX { get; set; }
+    public float PosY { get; set; }
+    public float SizeX { get; set; }
+    public float SizeY { get; set; }
+    public bool IsOnScreen { get; set; }
+    public ObjectType Type { get; set; } = ObjectType.SolidBlock;
+
+    public GameObject(float X, float Y, float SizX, float SizY, bool IsOnScr, ObjectType ObjType)
+    {
+      PosX = X;
+      PosY = Y;
+      SizeX = SizX;
+      SizeY = SizY;
+      IsOnScreen = IsOnScr;
+      Type = ObjType;
+    }
   }
 }
