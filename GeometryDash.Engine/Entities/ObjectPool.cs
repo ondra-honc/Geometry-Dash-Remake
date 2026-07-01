9831bcca1220e45dbc212455a2efb2ecc57d323e
Ondřej Honc <ondra.honc1@gmail.com>
2026-07-01 18:18:45 +0200

    feat(core): implement dynamic ObjectPool for game object recycling

--- GeometryDash.Engine/Entities/ObjectPool.cs
+++ GeometryDash.Engine/Entities/ObjectPool.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GeometryDash.Engine.Entities
+﻿namespace GeometryDash.Engine.Entities
 {
-  internal class ObjectPool
+  public class ObjectPool
   {
+    readonly Queue<Entities.GameObject> queue = new Queue<Entities.GameObject>();
+
+    public void InitializeQueue(int capacity)
+    {
+      for (int i = 0; i < capacity; i++)
+      {
+        Entities.GameObject obj = new Entities.GameObject();
+        queue.Enqueue(obj);
+      }
+    }
+
+    public Entities.GameObject Get()
+    {
+      return queue.Count == 0 ? new Entities.GameObject() : queue.Dequeue();
+    }
+
+    public void Return(Entities.GameObject obj)
+    {
+      obj.IsOnScreen = false;
+      queue.Enqueue(obj);
+    }
   }
 }
