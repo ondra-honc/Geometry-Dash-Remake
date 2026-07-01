098c3114cc641fa6f4c29ea858a522c6765b51c5
Ondřej Honc <ondra.honc1@gmail.com>
2026-07-01 16:00:56 +0200

    add: core properties for PlayerCube class

--- GeometryDash.Engine/Entities/PlayerCube.cs
+++ GeometryDash.Engine/Entities/PlayerCube.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GeometryDash.Engine.Core;
 
 namespace GeometryDash.Engine.Entities
 {
   internal class PlayerCube
   {
+    public float posX {  get; set; }
+    public float posY { get; set; }
+    public float velocityY {  get; set; }
+    public bool isGrounded { get; private set; }
+    public float width { get; } = 40f;
+    public float height { get; } = 40f;
+
+    private const float Gravity = 2000f;    
+    private const float JumpForce = -600f;  
+    private const float FloorY = 350f;
+
+    public PlayerCube(float startX, float startY)
+    {
+      posX = startX;
+      posY = startY;
+      velocityY = 0;
+      isGrounded = false;
+    }
   }
 }
