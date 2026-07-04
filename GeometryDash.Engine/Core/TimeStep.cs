45248d95e2df674e5e4d290403be7ff57d9d4b60
Ondřej Honc <ondra.honc1@gmail.com>
2026-07-04 16:08:58 +0200

    feat(core): implement start for fixed-time step accumulator

--- GeometryDash.Engine/Core/TimeStep.cs
+++ GeometryDash.Engine/Core/TimeStep.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics;
 
 namespace GeometryDash.Engine.Core
 {
-  internal class TimeStep
+  public class TimeStep
   {
+    private readonly Stopwatch _stopwatch = new Stopwatch();
+    private double _previousTime;
+    private double _accumulatedTime;
+    private readonly double _targetFrameTime;
+
+    public TimeStep(int targetFps)
+    {
+      _targetFrameTime = 1.0 / targetFps;
+    }
+
+    public void Start()
+    {
+      _stopwatch.Start();
+      _previousTime = _stopwatch.Elapsed.TotalSeconds;
+      _accumulatedTime = 0.0;
+    }
+
+    
   }
 }
