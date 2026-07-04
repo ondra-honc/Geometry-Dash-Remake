5f758d87c35d046b9e70a35df93de34364998f17
Ondřej Honc <ondra.honc1@gmail.com>
2026-07-04 16:18:13 +0200

    feat(core): implement main engine loop architecture

--- GeometryDash.Engine/Core/Engine.cs
+++ GeometryDash.Engine/Core/Engine.cs
@@ -1,10 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics;
 
 namespace GeometryDash.Engine.Core
 {
-  internal class Engine
+  public class Engine
   {
+    private bool isRunning;
+    private TimeStep timeStep;
+
+    public void Start()
+    {
+      timeStep = new TimeStep((int)(GameSettings.targetFrameRate));
+      timeStep.Start();
+      isRunning = true;
+
+      Run();
+    }
+
+    private void Run()
+    {
+      while (isRunning)
+      {
+        int updateTicks = timeStep.GetRequiredUpdateTicks();
+
+        for (int i = 0; i < updateTicks; i++)
+        {
+          Update(timeStep.FixedDeltaTime);
+        }
+
+        Render();
+      }
+    }
+
+    private void Update(float deltaTime)
+    {
+      //Physics, etc.
+    }
+
+    private void Render()
+    {
+      //Drawing logic
+    }
   }
 }
