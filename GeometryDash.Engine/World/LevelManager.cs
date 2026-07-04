2a1b540d672598e82eb8685a1f5eeaaca8529e38
Ondřej Honc <ondra.honc1@gmail.com>
2026-07-04 15:43:25 +0200

    feat: method for loading level data with predetermined structure

--- GeometryDash.Engine/World/LevelManager.cs
+++ GeometryDash.Engine/World/LevelManager.cs
@@ -13,6 +13,29 @@ namespace GeometryDash.Engine.World
 
   public class LevelManager
   {
-    
+    private readonly List<LevelData> levelData = new List<LevelData>();
+
+    public void LoadLevel(string filePath)
+    {
+      if (!File.Exists(filePath)) return;
+
+      levelData.Clear();
+
+      using (StreamReader reader = new StreamReader(filePath))
+      {
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+          if (string.IsNullOrWhiteSpace(line)) continue;
+
+          string[] token = line.Split(',');
+          if (!(token.Length >= 3)) continue;
+          if (int.TryParse(token[0], out int type) && float.TryParse(token[1], out float x) && float.TryParse(token[2], out float y))
+          {
+            levelData.Add(new LevelData { TypeId = type, X = x, Y = y  });
+          }
+        }
+      }
+    }
   }
 }
